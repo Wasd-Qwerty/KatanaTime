880fda3cd83860a6722dfb227fa9c158dd4eb1ea
Wasd-Qwerty <kylikov_yt@mail.ru>
2023-02-22 15:10:04 +0300

    Почти адекватно || Сохранился, чтобы все не испортить
    Надо проверять в какую сторону он ударился!!!! Ща сделаю

--- FruitNindja/Assets/Scripts/SliceMeshes-master/SliceListener.cs
+++ FruitNindja/Assets/Scripts/SliceMeshes-master/SliceListener.cs
@@ -1,12 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SliceListener : MonoBehaviour
 {
     public SlicerMeshes slicer;
-    private void OnTriggerEnter(Collider other)
+    
+    private BoxCollider _boxCollider;
+
+    [SerializeField] private Transform _startPoint;
+    public LayerMask layerMask;
+    public float maxDistance = 1;
+    [SerializeField] private Vector3 _extents = new Vector3(0.1f, 1f, 0.1f);
+    [SerializeField] private Transform _directionDebug;
+    
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     slicer.Touch();
+    // }
+    private void Start()
     {
-        slicer.isTouched = true;
+        _boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private void FixedUpdate()
+    {
+        DebugDraw();
+        var direction = transform.up;
+        _directionDebug.position = direction;
+        // Debug.Log("Direction: " + direction);
+        var hits = Physics.BoxCastAll(_boxCollider.bounds.center, _extents, direction, _boxCollider.transform.rotation, maxDistance, layerMask, QueryTriggerInteraction.Ignore);
+        foreach (var hit in hits)
+        {
+            Debug.Log("Ok let`s go");
+            slicer.Touch();
+        }
+        // try
+        // {
+        //     
+        // }
+        // catch (Exception e)
+        // {
+        //     Debug.Log(e.Message);
+        //     Debug.Break();
+        // }
+    }
+    private void DebugDraw()
+    {
+        ExtDebug.DrawBox(_boxCollider.bounds.center, _extents, _boxCollider.transform.rotation,
+            Color.blue);
     }
 }
--- FruitNindja/Assets/Scripts/SliceMeshes-master/SlicerMeshes.cs
+++ FruitNindja/Assets/Scripts/SliceMeshes-master/SlicerMeshes.cs
@@ -1,52 +1,83 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using EzySlice;
+using Unity.VisualScripting;
+
 public class SlicerMeshes : MonoBehaviour
 {
     public LayerMask sliceMask;
-    public bool isTouched;
+    GameObject upperHullGameobject, lowerHullGameobject;
+
+    
+    // public bool isTouched;
     public Material materialAfterSlice;
-    private void Update()
+    // private void Update()
+    // {
+    //     if (isTouched == true)
+    //     {
+    //         isTouched = false;
+    //
+    //         Collider[] objectsToBeSliced = Physics.OverlapBox(transform.position, new Vector3(1, 0.1f, 0.1f), transform.rotation, sliceMask);
+    //         
+    //         foreach (Collider objectToBeSliced in objectsToBeSliced)
+    //         {
+    //             GameObject upperHullGameobject, lowerHullGameobject;
+    //             Material sliceMaterial = null;
+    //             if (objectToBeSliced.gameObject.GetComponent<SlicableMaterial>() != null)
+    //             {
+    //                 sliceMaterial = objectToBeSliced.gameObject.GetComponent<SlicableMaterial>().sliceMaterial;
+    //             }
+    //             if (sliceMaterial != null)
+    //             {
+    //                 SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
+    //                 upperHullGameobject = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
+    //                 lowerHullGameobject = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
+    //             }
+    //             else
+    //             {
+    //                 SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
+    //                 upperHullGameobject = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
+    //                 lowerHullGameobject = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
+    //             }
+    //
+    //             upperHullGameobject.transform.position = objectToBeSliced.transform.position;
+    //             lowerHullGameobject.transform.position = objectToBeSliced.transform.position;
+    //
+    //             MakeItPhysical(upperHullGameobject);
+    //             MakeItPhysical(lowerHullGameobject);
+    //             
+    //             Destroy(upperHullGameobject, 3f);
+    //             Destroy(lowerHullGameobject, 3f);
+    //             Destroy(objectToBeSliced.gameObject);
+    //         }
+    //     }
+    // }
+
+
+    public void Touch()
     {
-        if (isTouched == true)
+        Collider[] objectsToBeSliced = Physics.OverlapBox(transform.position, new Vector3(1, 0.1f, 0.1f), transform.rotation, sliceMask);
+        if(objectsToBeSliced.Length != 0) Debug.Log(objectsToBeSliced.Length);
+        foreach (Collider objectToBeSliced in objectsToBeSliced)
         {
-            isTouched = false;
+           
+            Material sliceMaterial = null;
+            SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
+            upperHullGameobject = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
+            lowerHullGameobject = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
 
-            Collider[] objectsToBeSliced = Physics.OverlapBox(transform.position, new Vector3(1, 0.1f, 0.1f), transform.rotation, sliceMask);
-            
-            foreach (Collider objectToBeSliced in objectsToBeSliced)
-            {
-                GameObject upperHullGameobject, lowerHullGameobject;
-                Material sliceMaterial = null;
-                if (objectToBeSliced.gameObject.GetComponent<SlicableMaterial>() != null)
-                {
-                    sliceMaterial = objectToBeSliced.gameObject.GetComponent<SlicableMaterial>().sliceMaterial;
-                }
-                if (sliceMaterial != null)
-                {
-                    SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
-                    upperHullGameobject = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
-                    lowerHullGameobject = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
-                }
-                else
-                {
-                    SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
-                    upperHullGameobject = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
-                    lowerHullGameobject = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
-                }
+            upperHullGameobject.transform.position = objectToBeSliced.transform.position;
+            lowerHullGameobject.transform.position = objectToBeSliced.transform.position;
 
-                upperHullGameobject.transform.position = objectToBeSliced.transform.position;
-                lowerHullGameobject.transform.position = objectToBeSliced.transform.position;
-
-                MakeItPhysical(upperHullGameobject);
-                MakeItPhysical(lowerHullGameobject);
+            MakeItPhysical(upperHullGameobject);
+            MakeItPhysical(lowerHullGameobject);
                 
-                Destroy(upperHullGameobject, 3f);
-                Destroy(lowerHullGameobject, 3f);
-                Destroy(objectToBeSliced.gameObject);
-            }
+            Destroy(upperHullGameobject, 3f);
+            Destroy(lowerHullGameobject, 3f);
+            
+            Destroy(objectToBeSliced.gameObject);
         }
     }
-
     private void MakeItPhysical(GameObject obj)
     {
         obj.AddComponent<MeshCollider>().convex = true;
