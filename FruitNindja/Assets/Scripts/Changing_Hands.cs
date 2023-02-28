be65ca985913a6b8778110ab8fae116692c6dda1
Vladimir80Utkin <vova.utkin.04@mail.ru>
2023-02-28 14:42:38 +0300

    hyita

--- FruitNindja/Assets/Scripts/Changing_Hands.cs
+++ FruitNindja/Assets/Scripts/Changing_Hands.cs
@@ -8,20 +8,26 @@ public class Changing_Hands : MonoBehaviour
     [SerializeField] GameObject left_hand_pan;
     [SerializeField] GameObject right_hand_knife;
     [SerializeField] GameObject right_hand_pan;
+
+    bool isPressed;
     private void Update()
     {
         Changing();
+        //if (OVRInput.Axis1D.PrimaryIndexTrigger) isPressed = true;
+        //if (OVRInput.Axis1D.SecondaryIndexTrigger == 0) isPressed = false;
+        ///if (OVRInput.GetUp(OVRInput.Button.One)) isPressed = true;
+        //if (OVRInput.GetUp(OVRInput.Button.Two)) isPressed = false;
     }
     void Changing()
     {
-        if (Input.GetKeyUp(KeyCode.C))
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
         {
             left_hand_knife.SetActive(false);
             right_hand_knife.SetActive(true);
             left_hand_pan.SetActive(true);
             right_hand_pan.SetActive(false);
         }
-        if (Input.GetKeyUp(KeyCode.X))
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger, OVRInput.Controller.LTouch))
         {
             left_hand_knife.SetActive(true);
             right_hand_knife.SetActive(false);
