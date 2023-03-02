4cf3b28fda2492ad0bad65e71424b8c9256466c5
Wasd-Qwerty <kylikov_yt@mail.ru>
2023-03-02 18:42:56 +0300

    Все фрукты и не фрукты

--- FruitNindja/Assets/Scripts/MenuManager.cs
+++ FruitNindja/Assets/Scripts/MenuManager.cs
@@ -3,14 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject _gameOverUI;
+    [SerializeField] private GameObject _winUI;
+    [SerializeField] private GameObject _score;
+    [SerializeField] private GameObject _scoreText;
+    [SerializeField] private GameObject _winScore;
+    
+    
+    
     [SerializeField] private Changing_Hands _changingHands;
     private void Start()
     {
         _gameOverUI.SetActive(false);
+        _winUI.SetActive(false);
     }
 
     public void ShowGameOverUI()
@@ -18,6 +27,17 @@ public class MenuManager : MonoBehaviour
         _gameOverUI.SetActive(true);
         _changingHands.Death();
     }
+    public void ShowWinUI()
+    {
+        var cacheScore = _score.GetComponent<Text>().text;
+        _winScore.GetComponent<Text>().text = cacheScore;
+
+        _scoreText.SetActive(false);
+        _score.SetActive(false);
+        _winUI.SetActive(true);
+        _changingHands.Death();
+    }
+    
     public void Restart()
     {
         SceneManager.LoadScene(0);
--- FruitNindja/Assets/Scripts/TrackTime.cs
+++ FruitNindja/Assets/Scripts/TrackTime.cs
@@ -2,18 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrackTime : MonoBehaviour
 {
-    public float timeInst;
-    public float flightTime;
-    [SerializeField] private LayerMask sliceLayer;
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] private float _timeInSecond = 10;
+    [SerializeField] private MenuManager _menuManager;
+    [SerializeField] private Animator _cookAnimator;
+
+    [SerializeField] private Text TimeText; 
+    void Start()
     {
-        if(((1 << other.gameObject.layer) & sliceLayer) != 0)
+
+        StartCoroutine("Track");
+
+    }
+
+    private IEnumerator Track()
+    {
+        while (true)
         {
-            var timeNow = Time.time;
-            flightTime = timeNow - timeInst;
+            _timeInSecond -= 1;
+            TimeText.text = Convert.ToString(_timeInSecond);
+            if (_timeInSecond == 0)
+            {
+                _menuManager.ShowWinUI();
+                _cookAnimator.SetTrigger("win");
+                break;
+            }
+            yield return new WaitForSeconds(1);
         }
     }
 }
