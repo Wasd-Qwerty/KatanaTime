de75fa3d058f9accdeda7a49158bb57d208e9aac
Vladimir80Utkin <vova.utkin.04@mail.ru>
2023-03-02 14:41:31 +0300

    rotation

--- FruitNindja/Assets/Scripts/Rotation_Physics.cs
+++ FruitNindja/Assets/Scripts/Rotation_Physics.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class Rotation_Physics : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    Quaternion originRotation;
+    [SerializeField] float angleX;    
+    [SerializeField] float angleY;    
+    [SerializeField] float angleZ;
 
     void Update()
+    {        
+        Quaternion rotationX = Quaternion.AngleAxis(angleX, new Vector3(1, 0, 0));
+        Quaternion rotationY = Quaternion.AngleAxis(angleY, new Vector3(0, 1, 0));
+        Quaternion rotationZ = Quaternion.AngleAxis(angleZ, new Vector3(0, 0, 1));
+        transform.rotation *= rotationX * rotationY * rotationZ;
+    }
+    private void OnCollisionEnter(Collision collision)
     {
-        Quaternion rotationY = Quaternion.AngleAxis(-8, new Vector3(1, 1, 0));
-        transform.rotation *= rotationY;
+        Destroy(this);
     }
 }
