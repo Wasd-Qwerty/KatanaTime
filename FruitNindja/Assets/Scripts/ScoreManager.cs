f44c55dab44e250e3f847228fad4574fa7bf409d
Wasd-Qwerty <kylikov_yt@mail.ru>
2023-03-16 12:08:55 +0300

    Оценка

--- FruitNindja/Assets/Scripts/ScoreManager.cs
+++ FruitNindja/Assets/Scripts/ScoreManager.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
 using TMPro; 
@@ -9,6 +7,57 @@ public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _scoreText;
     [SerializeField] private HealthBar _healthBar;
+
+    [SerializeField] private CookController _cookController;
+    public int countOfSlicedObjects = 0;
+    
+    public string estimation = "S";
+    
+    private void Update()
+    {
+        CalculateEstimation();
+    }
+
+    private void CalculateEstimation()
+    {
+        var percentOfSlice = countOfSlicedObjects / _cookController.maxCountOfEdible * 100;
+        if (percentOfSlice > 99)
+        {
+            // превосходно
+            estimation = "S";
+        }
+        else if (percentOfSlice > 89)
+        {
+            // отлично
+            estimation = "A";
+        }
+        else if(percentOfSlice > 79)
+        {
+            // хорошо
+            estimation = "B";
+        }
+        else if (percentOfSlice > 69)
+        {
+            // удовлетворительно
+            estimation = "C";
+        }
+        else if(percentOfSlice > 59)
+        {
+            // достаточно (проходной балл)
+            estimation = "D";
+        }
+        else
+        {
+            // Миша, всё хуйня - давай по новой
+            estimation = "E";
+        }
+    }
+
+    public void Death()
+    {
+        Debug.Log("Помер. Ваша оценка: " + estimation);
+    }
+    
     public void IncreaseScore(double editNumber)
     {
         var cacheScore = Convert.ToDouble(_scoreText.text);
