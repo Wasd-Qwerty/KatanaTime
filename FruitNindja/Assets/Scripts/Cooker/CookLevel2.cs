45e52dee1b7841803e45223e8ca8040436d97b64
Wasd-Qwerty <kylikov_yt@mail.ru>
2023-03-23 14:41:42 +0300

    as

---
+++ FruitNindja/Assets/Scripts/Cooker/CookLevel2.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cooker
+{
+    public class CookLevel2 : MonoBehaviour
+    {
+        [Header("ExplosionSettings")] [SerializeField]
+        private List<GameObject> _gameObjectsForExplosion;
+
+        [SerializeField] private float _explosionForce;
+        [SerializeField] private Transform _explosionTransform;
+        [SerializeField] private float _explosionRadius;
+        [SerializeField] private float _upwardsModifier;
+        [SerializeField] private ForceMode _expMode;
+
+        [Header("TableSettings")] [SerializeField]
+        private Rigidbody _rbTable;
+
+        [SerializeField] private Vector3 _forcePosition;
+        [SerializeField] private float _force;
+        [SerializeField] private ForceMode _tableMode;
+
+        public void ThrowTable()
+        {
+            _rbTable.AddForce(_forcePosition * _force, _tableMode);
+            foreach (var gObject in _gameObjectsForExplosion)
+            {
+                gObject.AddComponent<MeshCollider>().convex = true;
+                gObject.AddComponent<Rigidbody>().AddExplosionForce(_explosionForce, _explosionTransform.position,
+                    _explosionRadius, _upwardsModifier, _expMode);
+                Destroy(gObject, 4);
+            }
+        }
+    }
+}
