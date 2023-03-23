ba9eb5b0bd7b5e47f5e88bf967d41e3a5b2291d6
Wasd-Qwerty <kylikov_yt@mail.ru>
2023-03-23 17:10:51 +0300

    s

--- FruitNindja/Assets/Scripts/SceneInfo.cs
+++ FruitNindja/Assets/Scripts/SceneInfo.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 [Serializable]
 public class SceneInfo
 {
+    public bool unlocked;
+    public GameObject zaserino;
     public string nameOfScene;
     public Text bestScore;
     public Text estimation;
+    
 }
